1f91ca839f7f1f643657988d5b7979d23aac618d
lianghongle <lianghongle2012@163.com>
2016-02-01 18:04:18 +0800

    Revert "update"
    This reverts commit d2ce5c9f5e4b314769250da2e3790ffc553a0446.

--- comein.php
+++ comein.php
@@ -1,19 +1,27 @@
-<?php
+<?php 
 
-$static_filePath = "static/";
-// echo $_SERVER["REQUEST_URI"];die;
-$get_path = str_replace("/", "", $_SERVER["REQUEST_URI"]);
-
-if (file_exists($static_filePath . $get_path)) {
-	include ($static_filePath . $get_path);
-	//进入静态资源
-} else {
-	if (file_exists("api/" . $get_path . ".service"))// 加载 controller
+    $Static_FilePath="static/";
+   // echo $_SERVER["REQUEST_URI"];
+    $get_path=str_replace("/","",$_SERVER["REQUEST_URI"]);
+	
+	if(file_exists($Static_FilePath.$get_path))
 	{
-		require ("functions");
-		require ("api/" . $get_path . ".service");
-		$class_name = $get_path . "Service";
-		$loadClass = new $class_name();
-		$loadClass -> run();
+		include($Static_FilePath.$get_path);//进入静态资源
 	}
-}
+	else 
+	{
+		if(file_exists("api/".$get_path.".service")) // 加载 controller
+		{
+			require("functions");
+			require("api/".$get_path.".service");
+			$class_name=$get_path."Service";
+			$loadClass=new $class_name();
+			$loadClass->run();
+		}
+	}
+	
+    
+	
+    
+     
+?>
